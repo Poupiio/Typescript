ff89469145edd2b98934ecbf8d9e9208fd4eb6c6
Elise <elise.maaiza33@gmail.com>
2024-03-14 13:15:44 +0100

    Début cours

---
+++ index.ts
@@ -0,0 +1,13 @@
+// Assignation d'une valeur de type number
+let num1 = 24;
+
+// Déclaration d'une variable de type number sans valeur (avec ou sans espaces)
+let num2 : number;
+
+// Déclaration d'une variable sans connaître son type
+let numOrString : number | string;
+// numOrString = 48;
+// numOrString = "48";
+
+// Tableaux
+let arr = ["chien", "chat", "poisson"];
